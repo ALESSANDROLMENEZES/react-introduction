f58518f1ed3a3927e46886a84acee679c5aed856
Alessandro Menezes <alessandroadm@live.com>
2020-07-08 18:05:44 -0300

    First commit

---
+++ src/components/basics/First.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+let msg = 'Welcome, User!';
+export default function () {
+  return (
+    <div>
+      <h2>Hello From first component</h2>
+      <p>{ msg }</p>
+    </div>
+  )
+}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import ReactDom from 'react-dom';
+import React from 'react';
+import First from './components/basics/First';
+import './index.css';
+
+ReactDom.render(
+  <First></First>,
+  document.getElementById('root')
+);
