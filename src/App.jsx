b3def3d385695a75740ae65180fb3071ec9cd4a6
Alessandro Menezes <alessandroadm@live.com>
2020-07-09 15:04:28 -0300

    Adding css style

--- src/App.jsx
+++ src/App.jsx
@@ -6,40 +6,40 @@ import FragmentA from './components/basics/FragmentA';
 import FragmentB from './components/basics/FragmentB';
 import Random from './components/basics/Randon';
 import Card from './components/layout/Card';
+import './App.css';
 
 export default () => (
   /* jshint ignore:start */
   <>
     <h1>React App</h1> 
+    <div className="Cards">
+      <Card title="#01A - Primeiro component">
+        <First />
+      </Card>
 
+      <Card title="#01B - Primeiro component com fragmento">
+        <FragmentA />
+      </Card>
 
-    <Card title="#01A - Primeiro component">
-      <First />
-    </Card>
+      <Card title="#02 - Fragmento">
+        <FragmentB />
+      </Card>
 
-    <Card title="#01B - Primeiro component com fragmento">
-      <FragmentA />
-    </Card>
+      <Card title="#03 - Condicional">
+        <Third nota={7} name="Gabriel" />
+      </Card>
 
-    <Card title="#02 - Fragmento">
-      <FragmentB />
-    </Card>
-
-    <Card title="#03 - Condicional">
-      <Third nota={7} name="Gabriel" />
-    </Card>
-
-    <Card title="#04 - Props">
-        <Secound
-      title="This is a title"
-      subtitle="This is a subtitle from origin"
-      description="lorem ipsum dalari ielo set an dolarem umos las lies"/>
-    </Card>
-    
-    <Card title="#05 - Random">
-      <Random min={1} max={10}/>
-    </Card>
-    
+      <Card title="#04 - Props">
+          <Secound
+        title="This is a title"
+        subtitle="This is a subtitle from origin"
+        description="lorem ipsum dalari ielo set an dolarem umos las lies"/>
+      </Card>
+      
+      <Card title="#05 - Random">
+        <Random min={1} max={10}/>
+      </Card>
+    </div>
   </>
   /* jshint ignore:end */
 );
