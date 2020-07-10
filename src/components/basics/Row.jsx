f5df4b8c4abf2c7d2d07bc2890fdea4bd8bce0ea
Alessandro Menezes <alessandroadm@live.com>
2020-07-10 01:47:05 -0300

    Refactoring

--- src/components/basics/Row.jsx
+++ src/components/basics/Row.jsx
@@ -1,13 +1,13 @@
-import React, { cloneElement } from 'react';
+import React from 'react';
 import './Row.css';
 
 export default props => (
     /* jshint ignore:start */
     <>
-        <tr className="Table-Tr">
+        <tr className="Table-Tr" key={props.id}>
             <td>{props.id}</td>
             <td>{props.description}</td>
-            <td>{props.price}</td>
+            <td>R$ {props.price.toFixed(2).replace('.',',')}</td>
         </tr>
     </>
     /* jshint ignore:end */
